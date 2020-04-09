8e5f7d4f42d4702d41062176ff30c90eaa2d5ddb
hina-raza <61477000+hina-raza@users.noreply.github.com>
2020-04-09 12:03:19 -0400

    Create groceries-list.component.ts

---
+++ src/app/groceries-list/groceries-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-groceries',
+    templateUrl: 'groceries-list.component.html'
+})
+
+export class GroceryListComponent{
+    pageTitle: string = 'Groceries List';
+}
