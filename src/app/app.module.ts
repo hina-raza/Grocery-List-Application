57f2d418a0020f348f8d6edb63c2310cada072cf
hina-raza <61477000+hina-raza@users.noreply.github.com>
2020-04-09 12:03:03 -0400

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GroceryListComponent } from './groceries-list/groceries-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GroceryListComponent
   ],
   imports: [
     BrowserModule
