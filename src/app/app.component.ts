6f05a173f328740df6519afb5a96a2dbf32d93c1
hina-raza <61477000+hina-raza@users.noreply.github.com>
2020-04-09 12:02:59 -0400

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+            <div>
+              <h1>{{pageTitle}}</h1>
+              <app-groceries></app-groceries>
+            </div>
+            `
 })
-export class AppComponent {
-  title = 'Web 304: Angular';
+
+export class AppComponent{
+  pageTitle: string = 'My Groceries to Manager';
 }
